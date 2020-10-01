963617b262bdc7d672b2b2c3fd6536b969473c62
Harsh Arora <65437489+MrHarshArora@users.noreply.github.com>
2020-10-01 21:02:02 +0530

    Update CyberGhostWeapon.sh

--- CyberGhostWeapon.sh
+++ CyberGhostWeapon.sh
@@ -24,13 +24,13 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet CyberGhostWeapon
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+toilet -f mono12 -F border CyberGhost
+echo -e "\e[4;34m This Bomber Was Created By CyberGhost\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: Cyb3r.gh02t.ha@gmail.com \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/CyberGhost \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -66,9 +66,9 @@ cd ..
 rm -rf  TBomb >> temp
 rm update.speedx >> temp
 rm temp
-chmod +x TBomb.sh
+chmod +x CyberGhostWeapon.sh
 fi
-echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m CyberGhostWeapon Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
@@ -79,7 +79,7 @@ clear
 echo -e "\e[1;33m"
 figlet TBomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border CyberGhost
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -90,13 +90,13 @@ echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
 echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
-echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
-echo "         [-] Ping At: http://wa.me/917600140353"
+echo "         [-] Mail At: cyb3r.gh02t.ha@gmail.com"
+echo -e "\e[1;33m      [*]  Harsh Aroar   \e[1;31m"
+echo "         [-] Ping At: http://wa.me/8791208510"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
 echo "         [-] Ping At: https://t.me/Rieltar"
 echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
-echo "         [-] Mail At: 0n1cOn3@gmx.ch"
+echo "         [-] Mail At: "
 echo ""
 echo ""
 echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
@@ -106,22 +106,22 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail:cyb3r.gh02t.ha@gmail.com \e[0m"
+echo -e "\e[1;32m      Harsh Arora \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/CyberGhost\e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
 elif [ $ch -eq 5 ];then
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet CyberGhostWeapon
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border CyberGhost
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: cyb3r.gh02t.ha@gmail.com \e[0m"
+echo -e "\e[1;32m        \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/CyberGhostWeapon \e[0m"
 echo " "
 exit 0
 else
